2b0fc71de784c8dac553577aa189c237c082be13
Noh Seungjun <137677773+ZeroZoa@users.noreply.github.com>
2025-12-23 22:54:11 +0900

    [level 2] Title: H-Index, Time: 11.98 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -5,26 +5,22 @@ class Solution {
         int answer = 0;
         int point = 0;
         Arrays.sort(citations);
-        point = citations[citations.length-1];
+        point = citations.length-1;
         
         while(true){
             int count = 0;
             for(int i=0;i<citations.length; i++){//길이 만큼 반복
-                if(citations[i]<point){//h번 이하로 인용된 논문이 나오면 count
+                if(citations[i]<citations[point]){//h번 이하로 인용된 논문이 나오면 count
                     count++;
                 }else{//h번 이상 인용된 논문이 나오면 break
                     break;
                 }
             }
-            if((citations.length-count)>=point&&count<=point){
-                answer = point;
+            if((citations.length-count)>=citations[point]&&count<=citations[point]){
+                answer = citations[point];
                 break;
             }else{
-                if(point==0){
-                    return 0;
-                }else{
-                    point--;
-                }
+                point--;
             }
         }
         return answer;

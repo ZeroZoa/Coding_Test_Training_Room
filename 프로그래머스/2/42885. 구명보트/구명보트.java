2cd2a703864730ab9dc4fd4e7700bf02036aa741
Noh Seungjun <137677773+ZeroZoa@users.noreply.github.com>
2026-01-08 16:14:00 +0900

    [level 2] Title: 구명보트, Time: 36.68 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,24 +3,40 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
+        int answer = 0;
         //오름차순 정렬
         Arrays.sort(people);
         List<Integer> list = Arrays.stream(people).boxed().collect(Collectors.toList());
         
-        int answer = 0;
-        int startIndex = 0;
+        int temp = list.size() - 1;
         int endIndex = list.size() - 1;
-        
-        while(startIndex <= endIndex){//list가 비거나 시작점과 끝점이 같아지면 종료
-            if(list.get(startIndex)+list.get(endIndex)<=limit){
-                startIndex++;
-                endIndex--;
-                answer++;
+        while(temp>0){
+            if(list.get(temp)>limit-list.get(endIndex)){
+                temp--;
             }else{
-                endIndex--;
-                answer++;
+                break;
             }
         }
+        
+        
+        int startIndex = temp;
+        
+        
+        while(!list.isEmpty() && startIndex < endIndex){//list가 비거나 시작점과 끝점이 같아지면 종료
+            if(list.get(startIndex)+list.get(endIndex)<=limit){
+                list.remove(startIndex);
+                list.remove(endIndex);
+                endIndex = endIndex - 2;
+                answer++;
+            }else{
+                if(startIndex>0){
+                    startIndex--;
+                }else{
+                    endIndex--;
+                }
+            }
+        }
+        answer = answer + list.size();
         return answer;
     }
 }
